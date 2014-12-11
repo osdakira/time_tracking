cdc89f500c3cc6cb70f4ec51410470b9f53951e0
Akira Osada <osd.akira@gmail.com>
2014-12-11 17:18:58 +0900

    関数化

--- mac/time_tracking.go
+++ mac/time_tracking.go
@@ -7,22 +7,25 @@ import (
 	"os/exec"
 )
 
+func osascript() string {
+	path, err := exec.LookPath("osascript")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return path
+}
+
 func main() {
 	cmd_str := `Tell Application "System Events"
 set frontApp to name of first application process whose frontmost is true
 end tell`
 
-	path, err := exec.LookPath("osascript")
-	if err != nil {
-		log.Fatal(err)
-	}
-	osascript := path
-	cmd := exec.Command(osascript, "-e", cmd_str)
+	cmd := exec.Command(osascript(), "-e", cmd_str)
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
-	err = cmd.Run()
+	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
