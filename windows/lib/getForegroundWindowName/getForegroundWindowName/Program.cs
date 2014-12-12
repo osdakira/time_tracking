ed99b0ffe897c981179bce074c062926cb69581c
akira osada <a.osada@mugenup.com>
2014-12-12 18:19:02 +0900

    c# project

---
+++ windows/lib/getForegroundWindowName/getForegroundWindowName/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace getForegroundWindowName
+{
+	class MainClass
+	{
+		[DllImport ("user32.dll")]
+		public static extern IntPtr GetWindowThreadProcessId (IntPtr hWnd, out uint ProcessId);
+
+		[DllImport ("user32.dll")]
+		private static extern IntPtr GetForegroundWindow ();
+
+		public static void Main (string[] args)
+		{
+			try {
+				string name = getForegroundWindowName.MainClass.GetActiveProcessFileName ();
+				System.Console.WriteLine (name);
+			} catch {
+			}
+		}
+
+		private static string GetActiveProcessFileName ()
+		{
+			IntPtr hwnd = GetForegroundWindow ();
+			uint pid;
+			GetWindowThreadProcessId (hwnd, out pid);
+			Process p = Process.GetProcessById ((int)pid);
+			return p.MainModule.FileName;
+		}
+	}
+}
